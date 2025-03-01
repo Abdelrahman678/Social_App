709e7ab3017f806d11340607241e8a6378ed55a1
abdelrahman hassan <abdelrahman.7hassan@gmail.com>
2025-03-01 17:53:43 +0200

    multer local middleware changes

--- src/Middleware/multer.middleware.js
+++ src/Middleware/multer.middleware.js
@@ -2,23 +2,24 @@ import multer from 'multer';
 import fs from 'fs';
 
 export const MulterLocal = (destinationPath = 'general', allowedExtensions = []) => {
-    // make sure that the folder exists
-    const destinationFolder = `Assets/${destinationPath}`; 
-    if(!fs.existsSync(destinationFolder)){
-        fs.mkdirSync(destinationFolder, {recursive: true});
-    }
+    // // make sure that the folder exists
+    // const destinationFolder = `Assets/${destinationPath}`; 
+    // if(!fs.existsSync(destinationFolder)){
+    //     fs.mkdirSync(destinationFolder, {recursive: true});
+    // }
+    
     // disk storage
     const storage = multer.diskStorage({
-        // destination 'required'
-        destination: function (req, file, cb) {
-            cb(null, destinationFolder)
-        },
-        // filename 'optional'
-        filename: function (req, file, cb) {
-            // console.log("file", file); //file before upload only pasrsed
-            const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
-            cb(null, uniqueSuffix + '__' + file.originalname)
-        }        
+        // // destination 'required'
+        // destination: function (req, file, cb) {
+        //     cb(null, destinationFolder)
+        // },
+        // // filename 'optional'
+        // filename: function (req, file, cb) {
+        //     // console.log("file", file); //file before upload only pasrsed
+        //     const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9);
+        //     cb(null, uniqueSuffix + '__' + file.originalname)
+        // }        
     })
     // file filter
     const fileFilter = (req, file, cb) => {
